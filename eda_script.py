7c18af32c72db677980e998d07cd2bda221fe635
Philip Akomolfe <akomolafephilip@gmail.com>
2025-07-22 14:05:37 +0100

    created eda-schedule

--- eda_script.py
+++ eda_script.py
@@ -1,13 +1,23 @@
 import sweetviz as sv
 import pandas as pd
 import os
+from pathlib import Path
 
-# Load your dataset
-df = pd.read_csv("data/processed/train/aqi_train_data_v1.csv")
+# Define the working dir.
+root = Path(__file__).parent
 
-# Generate the report
-report = sv.analyze(df)
+def show_eda():
+    # Load your dataset
+    df = pd.read_csv(os.path.join(root, 'data', 'processed', 'train', 'aqi_train_data_v1.csv'))
 
-# Save the report
-os.makedirs('eda', exists_ok=True)
-report.show_html("eda/eda_report.html")
+    # Generate the report
+    report = sv.analyze(df)
+
+    # Save the report
+    # os.makedirs('eda', exists_ok=True)
+    report.show_html("eda_report.html")
+
+if __name__ == "__main__":
+    # Output the EDA of the data..
+    show_eda()
+    # pass
